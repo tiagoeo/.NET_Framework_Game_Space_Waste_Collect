c09f2a63cc938b85b44b3a8c97f98ab2de3e0bb9
Tiagoeo <tiago@trattecinformatica.com.br>
2023-01-30 20:36:16 -0300

    Adicionar arquivos de projeto.

---
+++ Space Waste Collect/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Space_Waste_Collect
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmGame());
+        }
+    }
+}
---
+++ Space Waste Collect/frmGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Space_Waste_Collect
+{
+    public partial class frmGame : Form
+    {
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
